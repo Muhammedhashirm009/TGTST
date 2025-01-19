aa3260a812bbf4d40bd417ed83e789fb7aa1bc0e
Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>
2025-01-19 17:53:10 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,12 +11,12 @@ def is_enabled(value, default):
         return default
 
 SESSION = environ.get("SESSION", "forward bot")
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-BOT_TOKEN = environ["BOT_TOKEN"]
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", 0))
+API_ID = "25467884"
+API_HASH = "05a687da36243593d4eb624f46375341"
+BOT_TOKEN = "5768391364:AAGX0HT3JZpwf_EbLDwz-IaQcQwvzt45xb4"
+LOG_CHANNEL = "@h4xlogs1"
 PORT = int(environ.get("PORT", "8080"))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = 2139560486
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Joelkb/File-Forward-Bot")
-DB_URI = environ.get('DB_URI', "")
-DB_NAME = environ.get('DB_NAME', "")
+DB_URI = "mongodb+srv://hashir:CI78iFp3LCQM0GuZ@cluster0.rvkb8jf.mongodb.net/"
+DB_NAME = "cluster0"
